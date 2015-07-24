6262e17e4a5670d9c03837d510aa9134746ee2fe
Rodrigo Morais <morais.rm@gmail.com>
2015-07-24 07:02:18 -0300

    Adding the method movie to return all movies from rental store.

--- lib/rentalStore.rb
+++ lib/rentalStore.rb
@@ -1,7 +1,16 @@
+require 'set'
+
 class RentalStore
     attr_accessor :name, :id
 
     def initialize(name, id)
         @name = name
+        @id = id
+
+        @movieList = Set.new
+    end
+
+    def movies
+        @movieList
     end
 end
