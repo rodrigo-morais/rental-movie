e6199c9ee3f27702802d9b6c46e344501e9fb49d
Rodrigo Morais <morais.rm@gmail.com>
2015-07-24 07:54:26 -0300

    Adding the test to add_movie method in RetalStore class.

--- specs/rentalStore_spec.rb
+++ specs/rentalStore_spec.rb
@@ -1,4 +1,5 @@
 require './lib/rentalStore'
+require './lib/movie'
 
 describe RentalStore do
     before do
@@ -7,5 +8,12 @@ describe RentalStore do
 
     it "the rental store with no movies regtistered have to return none movie in the movie list" do
         expect(@rentalStore.movies.length).to eq(0)
+    end
+
+    it "the rental store have to have a movie in its movie list after one movie was registered" do
+        @movie = Movie.new "Back to the future", 1, "Action", 134
+        @rentalStore.add_movie(@movie)
+
+        expect(@rentalStore.movies.length).to eq(1)
     end
 end
