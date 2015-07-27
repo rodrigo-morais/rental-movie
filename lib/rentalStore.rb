c3756c5d8fd03159a7da8ef3c593a87d6a6fe87e
Rodrigo Morais <morais.rm@gmail.com>
2015-07-27 06:54:46 -0300

    Creating a list of clients in RentalStore class.

--- lib/rentalStore.rb
+++ lib/rentalStore.rb
@@ -8,13 +8,22 @@ class RentalStore
         @id = id
 
         @movieList = Set.new
+        @clientList = Set.new
     end
 
     def movies
         @movieList
     end
 
+    def clients
+        @clientList
+    end
+
     def add_movie(movie)
         @movieList << movie
+    end
+
+    def add_client(client)
+        @clientList << client
     end
 end
--- specs/rentalStore_spec.rb
+++ specs/rentalStore_spec.rb
@@ -10,10 +10,21 @@ describe RentalStore do
         expect(@rentalStore.movies.length).to eq(0)
     end
 
+    it "the rental store with no clients regtistered have to return none client in the client list" do
+        expect(@rentalStore.clients.length).to eq(0)
+    end
+
     it "the rental store have to have a movie in its movie list after one movie was registered" do
         @movie = Movie.new "Back to the future", 1, "Action", 134
         @rentalStore.add_movie(@movie)
 
         expect(@rentalStore.movies.length).to eq(1)
+    end
+
+    it "the rental store have to have a client in its client list after one client was registered" do
+        @client = Client.new "Rodrigo Morais", 1, "24th October Street", "5136845328"
+        @rentalStore.add_client(@client)
+
+        expect(@rentalStore.clients.length).to eq(1)
     end
 end
