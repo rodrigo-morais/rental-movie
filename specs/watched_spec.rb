755ee53ad9b4f6bf921bfac46733ea4f7482c322
Rodrigo Morais <morais.rm@gmail.com>
2015-07-28 06:33:16 -0300

    Improving the method to_s of Watched class to present duration with word minutes to make it more understanble.

--- specs/watched_spec.rb
+++ specs/watched_spec.rb
@@ -8,6 +8,6 @@ describe Watched do
     end
 
     it "to_s method from watched have to return the name of movie and how much was watched" do
-        expect(@watched.to_s()).to eq("Back to the future - 23")
+        expect(@watched.to_s()).to eq("Back to the future - 23 minutes")
     end
 end
