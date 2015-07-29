48e4f720e2d84e6fa137a767f78cdbbb1ad20b41
Rodrigo Morais <morais.rm@gmail.com>
2015-07-29 07:05:20 -0300

    Adding the interface with ende user to register a movie.

--- lib/main.rb
+++ lib/main.rb
@@ -1,5 +1,55 @@
+require './lib/rentalStore'
 require './lib/movie'
+require './lib/client'
 
-movie = Movie.new "Back to the future", 1, "Action", 134
+@rentalStore = RentalStore.new "Malaju", 0
 
-puts movie
+def clear
+    puts "\e[H\e[2J"
+end
+
+def main
+    clear
+    
+    puts 'Menu'
+    puts ' 1 - Register movie'
+    puts ' 2 - Register client'
+    puts ' 3 - Register movie watched by client'
+    puts ' 4 - Register movie watched by client'
+
+    print 'Choose an option: '
+end
+
+def addMovie
+    clear
+    
+    puts "What's the movies title?"
+    @title = gets
+
+    puts "What's the movies genre?"
+    @genre = gets
+
+    puts "What's the movies duration?"
+    @duration = gets.to_i
+
+    @movie = Movie.new @title, 0, @genre, @duration
+    @rentalStore.add_movie @movie
+
+    main
+end
+
+puts 'Rental Store'
+puts ''
+
+
+main
+
+@option = 0
+
+while @option != 4 do
+    @option = gets.to_i
+
+    case @option
+        when 1 then addMovie
+    end
+end
