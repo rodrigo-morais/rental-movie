0c15da96bf504e2d780af752e96da7da1b86d1dc
Rodrigo Morais <morais.rm@gmail.com>
2015-09-10 06:24:01 -0300

    Adding more unit tests.

--- specs/unit/rentalStore_spec.rb
+++ specs/unit/rentalStore_spec.rb
@@ -9,6 +9,7 @@ describe RentalStore do
         allow(repo).to receive(:get_clients).and_return([])
         allow(repo).to receive(:add_movie)
         allow(repo).to receive(:add_client)
+        allow(repo).to receive(:add_watched_movie)
         @rentalStore = RentalStore.new "Maluju", repo
     end
 
@@ -27,11 +28,141 @@ describe RentalStore do
         expect(@rentalStore.movies.length).to eq(1)
     end
 
+    it "the rental store with two movies in its movie list, when movie is looking for by name it has to be presented" do
+        @movie = Movie.new 0, "Back to the future", "Action", 134
+        @rentalStore.add_movie(@movie)
+
+        @movie = Movie.new 1, "Lord of Rings", "Fantasy", 528
+        @rentalStore.add_movie(@movie)
+
+        expect(@rentalStore.get_movie_by_name("Back to the future").name).to eq("Back to the future")
+    end
+
+    it "the rental store with two movies in its movie list, when movie is looking for by name which doesn't exist it has to be nil" do
+        @movie = Movie.new 0, "Back to the future", "Action", 134
+        @rentalStore.add_movie(@movie)
+
+        @movie = Movie.new 1, "Lord of Rings", "Fantasy", 528
+        @rentalStore.add_movie(@movie)
+
+        expect(@rentalStore.get_movie_by_name("Catch me if you can")).to be_nil
+    end
+
+    it "the rental store with two movies in its movie list, when verify if movie exists it has to be true" do
+        @movie = Movie.new 0, "Back to the future", "Action", 134
+        @rentalStore.add_movie(@movie)
+
+        @movie = Movie.new 1, "Lord of Rings", "Fantasy", 528
+        @rentalStore.add_movie(@movie)
+
+        expect(@rentalStore.exist_movie?("Lord of Rings")).to be true
+    end
+
+    it "the rental store with two movies in its movie list, when verify if movie exists, and it doesn't, it has to be falsy" do
+        @movie = Movie.new 0, "Back to the future", "Action", 134
+        @rentalStore.add_movie(@movie)
+
+        @movie = Movie.new 1, "Lord of Rings", "Fantasy", 528
+        @rentalStore.add_movie(@movie)
+
+        expect(@rentalStore.exist_movie?("Catch me if you can")).to be false
+    end
+
+    it "the rental store with two movies in its movie list, when movie is looking for by id it has to be presented" do
+        @movie = Movie.new 0, "Back to the future", "Action", 134
+        @rentalStore.add_movie(@movie)
+
+        @movie = Movie.new 1, "Lord of Rings", "Fantasy", 528
+        @rentalStore.add_movie(@movie)
+
+        expect(@rentalStore.get_movie_by_id(1).name).to eq("Lord of Rings")
+    end
+
+    it "the rental store with two movies in its movie list, when movie is looking for by id which doesn't exist it has to be nil" do
+        @movie = Movie.new 0, "Back to the future", "Action", 134
+        @rentalStore.add_movie(@movie)
+
+        @movie = Movie.new 1, "Lord of Rings", "Fantasy", 528
+        @rentalStore.add_movie(@movie)
+
+        expect(@rentalStore.get_movie_by_id(3)).to be_nil
+    end
+
+    it "the rental store with two movies in its movie list, verify if duration of a specif movie is valid. The duration passed is smaller than the dration of movie and the return have to be true" do
+        @movie = Movie.new 0, "Back to the future", "Action", 134
+        @rentalStore.add_movie(@movie)
+
+        @movie = Movie.new 1, "Lord of Rings", "Fantasy", 528
+        @rentalStore.add_movie(@movie)
+
+        expect(@rentalStore.movie_duration_valid?("Back to the future", 55)).to be true
+    end
+
+    it "the rental store with two movies in its movie list, verify if duration of a specif movie is valid. The duration passed is equal than the dration of movie and the return have to be true" do
+        @movie = Movie.new 0, "Back to the future", "Action", 134
+        @rentalStore.add_movie(@movie)
+
+        @movie = Movie.new 1, "Lord of Rings", "Fantasy", 528
+        @rentalStore.add_movie(@movie)
+
+        expect(@rentalStore.movie_duration_valid?("Back to the future", 134)).to be true
+    end
+
+    it "the rental store with two movies in its movie list, verify if duration of a specif movie is valid. The duration passed is bigger than the dration of movie and the return have to be false" do
+        @movie = Movie.new 0, "Back to the future", "Action", 134
+        @rentalStore.add_movie(@movie)
+
+        @movie = Movie.new 1, "Lord of Rings", "Fantasy", 528
+        @rentalStore.add_movie(@movie)
+
+        expect(@rentalStore.movie_duration_valid?("Back to the future", 135)).to be false
+    end
+
+    it "the rental store with two movies in its movie list, verify if duration of a movie which doesn't exist. The return have to be false" do
+        @movie = Movie.new 0, "Back to the future", "Action", 134
+        @rentalStore.add_movie(@movie)
+
+        @movie = Movie.new 1, "Lord of Rings", "Fantasy", 528
+        @rentalStore.add_movie(@movie)
+
+        expect(@rentalStore.movie_duration_valid?("First Love", 5)).to be false
+    end
+
     it "the rental store have to have a client in its client list after one client was registered" do
         @client = Client.new 0, "Rodrigo Morais", "October 24th Street", "5136845328"
         @rentalStore.add_client(@client)
 
         expect(@rentalStore.clients.length).to eq(1)
+    end
+
+    it "the rental store with two clients in its client list, when a client is looking for by name it has to be presented" do
+        @client = Client.new 0, "Rodrigo Morais", "October 24th Street", "5136845328"
+        @rentalStore.add_client(@client)
+
+        @client = Client.new 1, "Kathi", "Germany", "884566996666"
+        @rentalStore.add_client(@client)
+
+        expect(@rentalStore.get_client("Kathi").name).to eq("Kathi")
+    end
+
+    it "the rental store with two clients in its client list, verify if a existent client exist. The return have to be true" do
+        @client = Client.new 0, "Rodrigo Morais", "October 24th Street", "5136845328"
+        @rentalStore.add_client(@client)
+
+        @client = Client.new 1, "Kathi", "Germany", "884566996666"
+        @rentalStore.add_client(@client)
+
+        expect(@rentalStore.exist_client?("Kathi")).to be true
+    end
+
+    it "the rental store with two clients in its client list, verify if a doesn't existent client exist. The return have to be false" do
+        @client = Client.new 0, "Rodrigo Morais", "October 24th Street", "5136845328"
+        @rentalStore.add_client(@client)
+
+        @client = Client.new 1, "Kathi", "Germany", "884566996666"
+        @rentalStore.add_client(@client)
+
+        expect(@rentalStore.exist_client?("Carol")).to be false
     end
 
     it "should return a movie from movies list using the name to looking for it" do
@@ -60,5 +191,24 @@ describe RentalStore do
         @rentalStore.add_client(@client)
 
         expect(@rentalStore.get_client("Ana Paula")).to be nil
+    end
+
+    it "shouldn't return any watched movie from a new client" do
+        @client = Client.new 0, "Rodrigo Morais", "24th October Street", "5136845328"
+        @rentalStore.add_client(@client)
+
+        expect(@client.watchedMovies.length).to be(0)
+    end
+
+    it "should return one movie after add a watched movie to a client" do
+        @movie = Movie.new 0, "Back to the future", "Action", 134
+        @rentalStore.add_movie(@movie)
+
+        @client = Client.new 0, "Rodrigo Morais", "24th October Street", "5136845328"
+        @rentalStore.add_client(@client)
+
+        @rentalStore.watch_movie("Rodrigo Morais", "Back to the future", 33)
+
+        expect(@client.watchedMovies.length).to be(1)
     end
 end
